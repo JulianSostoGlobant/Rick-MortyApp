b80d4d3e63930ca2180c5ce518f2e58499b6014e
Julián Francisco Sosto <julian.sosto@globant.com>
2020-11-12 19:09:58 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormSearchComponent } from './shared/components/form-search/form-search.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormSearchComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
